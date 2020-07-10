c3f9092e278018c66571b77c02bc24ea3fedf486
Sebas <sebas.warsaw@gmail.com>
2020-07-10 14:51:32 -0300

    heroku

---
+++ build/precache-manifest.e5e78441e20ddfc0df9193d84538eb09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "506a45ff1ea9c955fcaf06d97c6daeba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb19342c04fba4ab2180",
+    "url": "/static/js/2.74ace1db.chunk.js"
+  },
+  {
+    "revision": "9c2b079c7c507ea3f72ef59109b49d3d",
+    "url": "/static/js/2.74ace1db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "119ad93d6dc30a66adab",
+    "url": "/static/js/main.c6dc687a.chunk.js"
+  },
+  {
+    "revision": "3ea630216d51dfd1d356",
+    "url": "/static/js/runtime-main.56e11b81.js"
+  },
+  {
+    "revision": "c187d98ee28b413faf7c73924e6e364c",
+    "url": "/static/media/chat.c187d98e.png"
+  }
+]);
